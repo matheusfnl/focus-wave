53c36aec2cac9a0342d0390dc0aa090612f8c560
Matheus Gabriel Goncalves Cunha De Oliveira <1362286@sga.pucminas.br>
2023-10-28 00:27:41 -0300

    hide key

--- assets/js/supabaseClient.js
+++ assets/js/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://bndvswdzxhfrfgreyfir.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJuZHZzd2R6eGhmcmZncmV5ZmlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgwMDk3NjMsImV4cCI6MjAxMzU4NTc2M30.7iO6GdfeXmUTJsi934xqiNpr-BKbDX-PIILRCtRSDI4'
+const SUPABASE_URL = 'need-supabase-url'
+const SUPABASE_KEY = 'need-supabase-key'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
